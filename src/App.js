ba9e98a01c23e3fbcd645246906ad7ac87a1bd36
Jason M Harrison <harrisonj@dnb.com>
2019-05-28 14:42:11 -0500

    Simplifying layout.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -27,8 +26,7 @@ state = {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={ logo } className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h2 className="App-title">D&B Configurable Log System</h2>
         </header>
         <p className="App-intro">{ this.state.data }</p>
       </div>
