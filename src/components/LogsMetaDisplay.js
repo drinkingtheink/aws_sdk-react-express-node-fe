c09b38dd03ad0c27d708de5b1e7258065620bbb1
Jason M Harrison <harrisonj@dnb.com>
2019-07-11 16:25:09 -0500

    Using countup in LogsMetaDisplay to display count of logs found.

--- src/components/LogsMetaDisplay.js
+++ src/components/LogsMetaDisplay.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import CountUp from 'react-countup';
 
 const LogsMetaDisplay = ({ logCount = 0, searchedTerm = null, searchStartTime = null}) => {
 	return(
 		<section className="logs-meta-display">
-			<p className="meta-field"><strong>{logCount}</strong> Logs Found</p>
+			<p className="meta-field"><strong><CountUp duration={ 1 } end={ logCount } /></strong> Logs Found</p>
 			{ searchedTerm 
-				? <p className="meta-field">Searching for <strong>"{searchedTerm}"</strong></p>
+				? <p className="meta-field">Searching for <strong>"{ searchedTerm }"</strong></p>
 				: null
 			}
 		</section>
